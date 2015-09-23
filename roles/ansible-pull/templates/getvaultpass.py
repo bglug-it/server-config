ca3fb6d218be62206588aa097439e6f18f279310
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-09-23 23:58:30 +0200

    Temporary fix on getvaultpass script.

--- roles/ansible-pull/templates/getvaultpass.py
+++ roles/ansible-pull/templates/getvaultpass.py
@@ -7,11 +7,19 @@ from base64 import b64decode
 from requests import get
 from socket import gethostname
 from tempfile import mkstemp
+import time
 import os
 
 key = MD5.new(gethostname()).digest()
 cipher = AES.new(key, AES.MODE_ECB)
-base64enc = get("http://{{ ansible_local.domain.serverfqdn }}:3000/vaultpass")
+
+for i in range(10):
+  try:
+    base64enc = get("http://{{ ansible_local.domain.serverfqdn }}:3000/vaultpass")
+    break
+  except:
+    time.sleep(1)
+
 encrypted = b64decode(base64enc.content)
 longpass = cipher.decrypt(encrypted)
 
