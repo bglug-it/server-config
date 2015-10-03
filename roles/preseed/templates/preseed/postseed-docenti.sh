85783747a4cb7193f37e401aff1ff6ad50706ca7
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-10-03 16:16:29 +0200

    Correcting some errors in postseed scripts and init-ansible script.

--- roles/preseed/templates/preseed/postseed-docenti.sh
+++ roles/preseed/templates/preseed/postseed-docenti.sh
@@ -3,7 +3,7 @@
 # Cambio username con quello fornito da servizio
 TARGETHOSTNAME=$(wget -qO- http://{{ ansible_local.domain.serverfqdn }}:3000/whatsmyhostname?role=docenti)
 echo $TARGETHOSTNAME > /etc/hostname
-sed -i '' -e "s/kickseed/$TARGETHOSTNAME/g" /etc/hosts
+sed -i'' -e "s/kickseed/$TARGETHOSTNAME/g" /etc/hosts
 
 # Installo i file necessari per ansible.
 curl -sSL http://{{ ansible_local.domain.serverfqdn }}/ks/ansible-pull/init-ansible.sh | bash
