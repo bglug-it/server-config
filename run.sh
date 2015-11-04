28b4aa5f7581ff41421f53e88df584bf2faa751b
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-11-04 23:52:57 +0100

    Committing changes for mirror mode selection.

--- run.sh
+++ run.sh
@@ -8,10 +8,6 @@ echo "Please review settings for domain/server settings."
 sleep 2
 $EDITOR domain.yml
 
-echo "Please review mirror settings from mirror.yml."
-sleep 2
-$EDITOR mirror.yml
-
 # Configure the server machine using Ansible tasks.
-ansible-playbook init.yml --ask-pass
+ansible-playbook init.yml --ask-pass -e admin_sshkey=$HOME/.ssh/id_rsa.pub
 ansible-playbook setup.yml
