49d16a83ee17715eb103650cdd7c5e83c447c886
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2016-09-13 01:51:07 +0200

    Adding files for a new role: PCs for the classrooms.

---
+++ roles/preseed/templates/preseed/postseed-aula.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Cambio username con quello fornito da servizio
+TARGETHOSTNAME=$(/usr/bin/wget -qO- http://{{ ansible_local.domain.serverfqdn }}:3000/whatsmyhostname?role=aula)
+echo $TARGETHOSTNAME > /etc/hostname
+/bin/sed -i'' -e "s/kickseed/$TARGETHOSTNAME/g" /etc/hosts
+
+# Installo i file necessari per ansible.
+/usr/bin/curl -sSL4 http://{{ ansible_local.domain.serverfqdn }}/ks/ansible-pull/init-ansible.sh | /bin/bash
